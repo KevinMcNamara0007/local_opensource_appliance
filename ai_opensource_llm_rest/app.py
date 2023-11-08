9ef3826621cea22e3a2ed2273d478b8416f38af5
G Vamshi Krishna <vamshikrishnagvk5@gmail.com>
2023-11-08 23:23:48 +0530

    Add files via upload
    multi-model swagger and html

--- ai_opensource_llm_rest/app.py
+++ ai_opensource_llm_rest/app.py
@@ -7,24 +7,12 @@ from flask_restx import Api, Resource, fields
 from flask_cors import CORS
 
 # Instruction model
-model = "/Users/loki/Desktop/worksurface/ai_lab/models/mistral_quantized/mistral-7b-instruct-v0.1.Q2_K.gguf"
+model_mistralAI1_gguf = "C:/Users/Vamshi Krishna Gundu/Desktop/Vamshi/LnD/Data_Science/Projects/llm_models/mistral-7b-instruct-v0.1.Q2_K.gguf"
+model_mistralAI2_gguf = "C:/Users/Vamshi Krishna Gundu/Desktop/Vamshi/LnD/Data_Science/Projects/llm_models/mistral-7b-instruct-v0.1.Q2_K.gguf"
 #model = "/Users/loki/Desktop/worksurface/ai_lab/models/zephyr_quantized/zephyr-7b-alpha.Q2_K.gguf"
 
-app = Flask(__name__)
-api = Api(app)
-api = CORS(app)
-
-# Enable Cross-Origin Resource Sharing (CORS) for all routes and origins.
-#CORS(app, resources={r"/*": {"origins": "*"}})
-
-ns = api.namespace("mistral", description="mistralAI")
-
-message_model = api.model("Message", {'message': fields.String(description="Input message")})
-
-processed_model = api.model("ProcessedMessage", {"result": fields.String(description="Mistral output")})
-
-llm = Llama(
-    model_path=model,
+llm_mistralAI1 = Llama(
+    model_path=model_mistralAI1_gguf,
     n_ctx=8192,
     n_batch=512,
     n_threads=7,
@@ -33,22 +21,58 @@ llm = Llama(
     seed=42,
 )
 
-@app.route('/')
-def index():
-    return render_template("index.html")
 
-@ns.route("/process_message")
-class Generate(Resource):
-    @api.expect(message_model)
-    @api.marshal_with(processed_model)
+llm_mistralAI2 = Llama(
+    model_path=model_mistralAI2_gguf,
+    n_ctx=8192,
+    n_batch=512,
+    n_threads=7,
+    n_gpu_layers=2,
+    verbose=True,
+    seed=42,
+)
+
+app = Flask(__name__)
+api = Api(app)
+CORS(app)  # Enable CORS for the API
+
+# Create a namespace for the API
+ns = api.namespace('mistral', description='Text conv with mistral')
+
+# Define a model for the input data
+input_model = api.model('InputModel', {'message': fields.String(description='Text to be passed to model')})
+
+@ns.route('/mistralAI1/process_message')
+class Model1ProcessMessage(Resource):
+    @api.expect(input_model, validate=True)
     def post(self):
-        message = request.json["message"]
-        output = llm(message, echo=True, stream=False, max_tokens=4096)
+        # Parse the input data
+        data = request.json
+        message = data.get('message', '')
+        
+        output = llm_mistralAI1(message, echo=True, stream=False, max_tokens=4096)
         print(f"-------- output from mistral: {output['choices'][0]['text']} -------")
 
         return {
             "result": output['choices'][0]['text']
         }
+    
+
+@ns.route('/mistralAI2/process_message')
+class Model2ProcessMessage(Resource):
+    @api.expect(input_model, validate=True)
+    def post(self):
+        # Parse the input data
+        data = request.json
+        message = data.get('message', '')
+        
+        output = llm_mistralAI2(message, echo=True, stream=False, max_tokens=4096)
+        print(f"-------- output from mistral: {output['choices'][0]['text']} -------")
+
+        return {
+            "result": output['choices'][0]['text']
+        }
+    
 
 # Start the API server
 if __name__ == "__main__":
