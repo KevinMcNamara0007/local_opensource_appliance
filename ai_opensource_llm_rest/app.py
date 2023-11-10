2fcb0e83f90d3235a506f9ab65accfc2c7a41f0a
Kevin McNamara <kevin@mcnamara-group.com>
2023-11-10 09:46:44 -0500

    Edit Rest & Index.html take 2and push to repository by Kevin Mc

--- ai_opensource_llm_rest/app.py
+++ ai_opensource_llm_rest/app.py
@@ -1,35 +1,30 @@
-import json
-import requests
-from llama_cpp import Llama
 from flask import Flask, request, jsonify, render_template, redirect, url_for
-from flask_swagger_ui import get_swaggerui_blueprint
 from flask_restx import Api, Resource, fields
 from flask_cors import CORS
+from llama_cpp import Llama
 
-# Instruction model
-model_mistral7b = "/Users/loki/Desktop/worksurface/ai_lab/models/mistral_quantized/mistral-7b-instruct-v0.1.Q2_K.gguf"
-model_zephyr7b = "/Users/loki/Desktop/worksurface/ai_lab/models/zephyr_quantized/zephyr-7b-alpha.Q2_K.gguf"
+# Model paths
+model_paths = [
+    "/Users/loki/Desktop/worksurface/ai_lab/models/mistral_quantized/mistral-7b-instruct-v0.1.Q2_K.gguf",
+    "/Users/loki/Desktop/worksurface/ai_lab/models/zephyr_quantized/zephyr-7b-alpha.Q2_K.gguf",
+    "/Users/loki/Desktop/worksurface/ai_lab/models/zephyr_quantized/zephyr-7b-alpha.Q2_K.gguf",
+]
 
-llm_mistralAI1 = Llama(
-    model_path=model_mistral7b,
-    n_ctx=8192,
-    n_batch=512,
-    n_threads=7,
-    n_gpu_layers=2,
-    verbose=True,
-    seed=42,
-)
+# Create a list of LLMs
+llms = []
+for model_path in model_paths:
+    llm = Llama(
+        model_path=model_path,
+        n_ctx=8192,
+        n_batch=512,
+        n_threads=7,
+        n_gpu_layers=2,
+        verbose=True,
+        seed=42,
+    )
+    llms.append(llm)
 
-llm_mistralAI2 = Llama(
-    model_path=model_zephyr7b,
-    n_ctx=8192,
-    n_batch=512,
-    n_threads=7,
-    n_gpu_layers=2,
-    verbose=True,
-    seed=42,
-)
-
+# Create a Flask app
 app = Flask(__name__)
 api = Api(app)
 CORS(app)  # Enable CORS for the API
@@ -37,34 +32,23 @@ CORS(app)  # Enable CORS for the API
 # Create a namespace for the API
 ns = api.namespace('mistral', description='Text conv with mistral')
 
-# Define a model for the input data
-input_model = api.model('InputModel', {'message': fields.String(description='Text to be passed to model')})
-
-@ns.route('/mistralAI1/process_message')
-class Model1ProcessMessage(Resource):
-    @api.expect(input_model, validate=True)
+@ns.route('/process_message', methods=['POST'])
+class ProcessMessage(Resource):
+    @ns.doc(params={'message': 'Text to be passed to model', 'model': 'Model number'})
+    #@api.expect(input_model, validate=True)
     def post(self):
         # Parse the input data
-        data = request.json
-        message = data.get('message', '')
+        #data = request.json
+        message = request.args.get('message', request.form.get('message', ''))
+        model = request.args.get('model', request.form.get('model', '0'))
+        print("request form -------->", request)
+        print("I am printing this guy --------> ", message, model)
 
-        output = llm_mistralAI1(message, echo=True, stream=False, max_tokens=4096)
-        print(f"-------- output from mistral: {output['choices'][0]['text']} -------")
+        # Select an LLM
+        llm = llms[int(model)]
 
-        return {
-            "result": output['choices'][0]['text']
-        }
-
-
-@ns.route('/mistralAI2/process_message')
-class Model2ProcessMessage(Resource):
-    @api.expect(input_model, validate=True)
-    def post(self):
-        # Parse the input data
-        data = request.json
-        message = data.get('message', '')
-
-        output = llm_mistralAI2(message, echo=True, stream=False, max_tokens=4096)
+        # Generate a response
+        output = llm(message, echo=True, stream=False, max_tokens=4096)
         print(f"-------- output from mistral: {output['choices'][0]['text']} -------")
 
         return {
