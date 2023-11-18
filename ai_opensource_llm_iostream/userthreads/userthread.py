70b88f82fbe98bef71b66920eaa492502e5f4d30
Arpit Verma <arpitmanit.01@gmail.com>
2023-11-18 14:04:38 +0530

    Added setter and appender

--- ai_opensource_llm_iostream/userthreads/userthread.py
+++ ai_opensource_llm_iostream/userthreads/userthread.py
@@ -5,7 +5,7 @@ class UserThread:
     def __init__(self, identifier: Tuple[str, int]):
         self._identifier = identifier
         self._chat_model_name: Optional[str] = None
-        self._chat_list: Optional[List[str]] = None
+        self._chat_list: List[str] = []
 
     @property
     def identifier(self) -> Tuple[str, int]:
@@ -15,6 +15,22 @@ class UserThread:
     def chat_model_name(self) -> Optional[str]:
         return self._chat_model_name
 
+    @chat_model_name.setter
+    def chat_model_name(self, preferred_model_name):
+        if not (isinstance(preferred_model_name, str)):
+            raise TypeError('Preferred Model Name should be string')
+        else:
+            self._chat_model_name = preferred_model_name
+
     @property
     def chat_list(self) -> Optional[List[str]]:
         return self._chat_list
+
+    def append_chat_list(self, text: str) -> bool:
+        try:
+            self._chat_list.append(text)
+            return True
+        except Exception as e:
+            print("[Exception] Occurred")
+            print(e)
+            return False
